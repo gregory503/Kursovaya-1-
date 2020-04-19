095cd8f65831826315cb56de14197f00303bd47d
gregory503 <55867905+gregory503@users.noreply.github.com>
2020-04-19 17:17:40 +0300

    Update Bubble.py

--- Bubble.py
+++ Bubble.py
@@ -9,7 +9,8 @@ def bubble_sort(nums):
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
                 # Set the flag to True so we'll loop again
                 swapped = True
-# Verify it works
-random_list_of_nums = [2, 3, 7, 11, 15, 22, 77, 98, 112, 55, 66, 17, 98, 18, 27, 29, 37, 48, 31, 58]  
-bubble_sort(random_list_of_nums)  
-print(random_list_of_nums) 
+import random
+arry = [random.randint(0, 1000) for i in range(1000)]
+print(arry)
+bubble_sort(arry)
+print(arry)
