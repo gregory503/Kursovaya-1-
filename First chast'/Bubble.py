668d4623cc77c3c9906221d6e5ec7590bf34cf56
gregory503 <55867905+gregory503@users.noreply.github.com>
2020-04-19 17:41:33 +0300

    Add files via upload

---
+++ First chast'/Bubble.py
@@ -0,0 +1,16 @@
+def bubble_sort(nums):  
+    # We set swapped to True so the loop looks runs at least once
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                # Swap the elements
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Set the flag to True so we'll loop again
+                swapped = True
+import random
+arry = [random.randint(0, 10000) for i in range(10000)]
+print(arry)
+bubble_sort(arry)
+print(arry)
